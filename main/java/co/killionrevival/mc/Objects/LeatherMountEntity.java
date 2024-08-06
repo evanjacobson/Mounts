504b1a3822e997ba8b271e8d357beb67a2a34b8e
Evan Jacobson <evanjacobson3@gmail.com>
2024-08-06 13:30:51 -0600

    HM-1: Add attribute field names, flesh out method to set attributes, add some fields to horse props

--- main/java/co/killionrevival/mc/Objects/LeatherMountEntity.java
+++ main/java/co/killionrevival/mc/Objects/LeatherMountEntity.java
@@ -1,6 +1,18 @@
 package co.killionrevival.mc.Objects;
 
+import co.killionrevival.mc.Interfaces.IHorse;
 import co.killionrevival.mc.Interfaces.IMountEntity;
+import org.bukkit.entity.AbstractHorse;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Horse;
 
-public class LeatherMountEntity {
+import static co.killionrevival.mc.Interfaces.IMountEntity.MovementSpeed;
+
+public class LeatherMountEntity extends MountEntity {
+
+    public LeatherMountEntity() {
+        super(EntityType.HORSE);
+
+        setAttributeValue(this.MovementSpeedAttribute, 10);
+    }
 }
--- main/java/co/killionrevival/mc/Objects/MountEntity.java
+++ main/java/co/killionrevival/mc/Objects/MountEntity.java
@@ -1,12 +1,58 @@
 package co.killionrevival.mc.Objects;
 
+import co.killionrevival.mc.Attributes.EntityAttribute;
 import co.killionrevival.mc.Interfaces.IMountEntity;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.AbstractHorse;
+import org.bukkit.entity.EntityType;
+import org.slf4j.event.Level;
+
+import java.io.Console;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 public class MountEntity implements IMountEntity {
 
+    public AbstractHorse Horse;
+    public EntityType HorseType;
 
-    //public MountEntity(String name, )
+    public MountEntity(EntityType horseType){
+        HorseType = horseType;
+    }
+    
+    @Override
+    public void setAttributeValue(String fieldName, double value) {
 
+        if(!isSpawned()){
+            Bukkit.getServer().getConsoleSender().sendMessage("The horse must exist to have attributes!");
+            return;
+        }
 
+        Field field;
+
+        try{
+            field = IMountEntity.class.getDeclaredField(fieldName);
+        }
+        catch(NoSuchFieldException e){
+            String errorMessage = "Error setting attribute value: " + e.getMessage() + "Stack Trace: " + Arrays.toString(e.getStackTrace());
+            Bukkit.getServer().getConsoleSender().sendMessage(errorMessage);
+            return;
+        }
+
+        var attr = field.getAnnotation(EntityAttribute.class).k();
+
+        Objects.requireNonNull(Horse.getAttribute(attr)).setBaseValue(value);
+    }
+
+    public AbstractHorse getHorseEntity(){
+        return isSpawned()
+                ? Horse
+                : null;
+    }
+
+    private boolean isSpawned(){
+        return Horse != null;
+    }
 }
